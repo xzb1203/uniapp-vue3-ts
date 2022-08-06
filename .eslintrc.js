a19d686394084f330e181cdc316cf4602ddb1c89
xuzhaobin <870110664.com>
2022-08-06 22:24:51 +0800

    eslint,prettier配置

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  globals: {
+    defineEmits: true,
+    document: true,
+    localStorage: true,
+    GLOBAL_VAR: true,
+    window: true,
+    defineProps: true,
+    defineExpose: true,
+  },
+  extends: [
+    './.eslintrc-auto-import.json',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended', // 添加 prettier 插件
+  ],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    jsxPragma: 'Vue',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['vue', '@typescript-eslint', 'import'],
+  rules: {
+    'no-undef': 'off',
+    'no-plusplus': 'off',
+    'no-console': 'off',
+    'no-unused-expressions': 'off',
+    'no-use-before-define': 'off',
+    'no-nested-ternary': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'vue/no-v-html': 'off',
+    'vue/comment-directive': [0],
+    'vue/multi-word-component-names': [0],
+    'vue/valid-v-for': 'off',
+    'vue/require-v-for-key': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'no-debugger': 'off',
+  },
+}

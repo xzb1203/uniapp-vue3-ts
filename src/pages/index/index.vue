72284127f1799e83ad807e0e3182b327500e7c6b
xuzhaobin <870110664.com>
2022-08-06 22:37:15 +0800

    feat: 完成pinia配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,12 +3,15 @@
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
+      <text>{{ test }}</text>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { useUserStore } from '@/store/modules/login';
+import { ref, computed } from 'vue';
+const test = computed(() => useUserStore().test);
 const title = ref('Hello');
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), // 把 @ 指向到 src 目录去
+    },
+  },
 });
